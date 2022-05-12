7a0a52f699562f7291d2b01e7540bfd8760d0ee6
hanmango-o <hanmango.o@gmail.com>
2022-05-12 15:27:32 +0900

    add: ui 추가 및 route 설정
    필요 ui를 추가하고 각 화면별 route를 mapping함
    mapping정보는 asset/data/service.dart에 작성함

---
+++ lib/asset/data/service.dart
@@ -0,0 +1,10 @@
+class Service {
+  static String SPLASH_ROUTE = '/';
+  static String HOME_ROUTE = '/home';
+  static String SIGN_IN_ROUTE = '/sign/in';
+  static String SIGN_UP_ROUTE = '/sign/up';
+  static String ADD_FRIEND_ROUTE = '/add-friend';
+  static String CHAT_LIST_ROUTE = '/chat/list';
+  static String CHAT_ROOM_ROUTE = 'chat/room/read';
+  static String CREATE_CHAT_ROOM_ROUTE = 'chat/room/create';
+}
---
+++ lib/ui/view/add_friend_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddFriendScreen extends StatefulWidget {
+  const AddFriendScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddFriendScreen> createState() => _AddFriendScreenState();
+}
+
+class _AddFriendScreenState extends State<AddFriendScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
