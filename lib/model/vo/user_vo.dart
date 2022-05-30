65b85e904ff10cb30929c6ba07f3e9569d4a2971
hanmango-o <hanmango.o@gmail.com>
2022-05-30 14:21:38 +0900

    ADD: user vo 추가
    사용자 정보를 담는 user vo 추가함

---
+++ lib/model/vo/user_vo.dart
@@ -0,0 +1,34 @@
+class UserVO {
+  late String user_id;
+  late String? user_name;
+  late int? user_sid;
+  late String? user_dept;
+
+  UserVO({
+    required this.user_id,
+    this.user_name,
+    this.user_sid,
+    this.user_dept,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'user_id': user_id,
+      'user_name': user_name,
+      'user_sid': user_sid,
+      'user_dept': user_dept,
+    };
+  }
+
+  UserVO.fromMap(Map<String, dynamic> map) {
+    user_id = map['user_id'];
+    user_name = map['user_name'];
+    user_sid = map['user_sid'];
+    user_dept = map['user_dept'];
+  }
+
+  @override
+  String toString() {
+    return '{user_id:$user_id, user_name:$user_name, user_sid:$user_sid, user_dept:$user_dept}';
+  }
+}
